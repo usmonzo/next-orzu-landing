dd6b8bb3d15585e816d502a342835c7b81607e83
usmonzo <turaef.usmon@gmail.com>
2023-05-26 22:34:32 +0500

    test: animation [#1];

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,72 @@
-import white from '../../../public/assets/whiteCard.webp';
+"use client";
 
-import black from '../../../public/assets/blackCard.webp';
-import PrimaryButton from '../Buttons/PrimaryButton';
+import white from "../../../public/assets/whiteCard.webp";
+import black from "../../../public/assets/blackCard.webp";
+import PrimaryButton from "../Buttons/PrimaryButton";
 
-import styles from './Header.module.scss';
-import Image from 'next/image';
-import Navbar from '../Navbar/Navbar';
-import HeaderContent from '../HeaderContent/HeaderContent';
+import styles from "./Header.module.scss";
+import Image from "next/image";
+import Navbar from "../Navbar/Navbar";
+import HeaderContent from "../HeaderContent/HeaderContent";
+import { motion } from "framer-motion";
 
 const Header = () => {
-	return (
-		<>
-			<header className={styles.header_container}>
-				<h1 className={styles.header_title}>
-					Выгодный кредит для <br />
-					<span className={styles.text_gradient}>покупки бытовой техники</span>
-				</h1>
-				<p className={styles.header_paragraph}>Получите одобрение в течении дня и получайте деньги, не посещая офис</p>
-				<PrimaryButton text="Получить кредит" />
-				<div className={styles.header_icons_container}>
-					<Image src={white} alt="2" width={1650} height={800} className={styles.header_icons} />
-					<Image src={black} alt="2" width={1650} height={800} className={styles.header_icons} />
-				</div>
-			</header>
-			<section className={styles.header_content_section}>
-				<div className={styles.header_content_color}>
-					<HeaderContent />
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <header className={styles.header_container}>
+        <motion.h1
+          className={styles.header_title}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.1 }}
+        >
+          Выгодный кредит для <br />
+          <span className={styles.text_gradient}>покупки бытовой техники</span>
+        </motion.h1>
+        <motion.p
+          className={styles.header_paragraph}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.1 }}
+        >
+          Получите одобрение в течении дня и получайте деньги, не посещая офис
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.1 }}
+        >
+          <PrimaryButton text="Получить кредит" />
+        </motion.div>
+        <motion.div
+          className={styles.header_icons_container}
+          initial={{ margin: 20, opacity: 0 }}
+          whileInView={{ margin: 0, opacity: 1 }}
+          transition={{ delay: 0.1 }}
+        >
+          <Image
+            src={white}
+            alt="2"
+            width={1650}
+            height={800}
+            className={styles.header_icons}
+          />
+          <Image
+            src={black}
+            alt="2"
+            width={1650}
+            height={800}
+            className={styles.header_icons}
+          />
+        </motion.div>
+      </header>
+      <section className={styles.header_content_section}>
+        <div className={styles.header_content_color}>
+          <HeaderContent />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Header;
--- src/components/Merchants/Merchants.tsx
+++ src/components/Merchants/Merchants.tsx
@@ -1,87 +1,180 @@
-'use client';
-import './Merchants.scss';
-import WhiteButton from '../Buttons/WhiteButton';
-import MerchBox from '../UI/MerchBox';
-import tech from '../../../public/assets/Vector.png';
-import phone from '../../../public/assets/Vector1.png';
-import clothes from '../../../public/assets/Vector2.png';
-import couch from '../../../public/assets/Vector3.png';
-import comp from '../../../public/assets/Vector4.png';
-import ring from '../../../public/assets/Vector5.png';
-import food from '../../../public/assets/Vector6.png';
-import parfum from '../../../public/assets/Vector7.png';
-import halmet from '../../../public/assets/Vector8.png';
-import tea from '../../../public/assets/Vector9.png';
-import gift from '../../../public/assets/Vector10.png';
-import medicine from '../../../public/assets/Vector11.png';
-import azs from '../../../public/assets/Vector12.png';
-import education from '../../../public/assets/Vector13.png';
-import child from '../../../public/assets/Vector14.png';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+"use client";
+import "./Merchants.scss";
+import WhiteButton from "../Buttons/WhiteButton";
+import MerchBox from "../UI/MerchBox";
+import tech from "../../../public/assets/Vector.png";
+import phone from "../../../public/assets/Vector1.png";
+import clothes from "../../../public/assets/Vector2.png";
+import couch from "../../../public/assets/Vector3.png";
+import comp from "../../../public/assets/Vector4.png";
+import ring from "../../../public/assets/Vector5.png";
+import food from "../../../public/assets/Vector6.png";
+import parfum from "../../../public/assets/Vector7.png";
+import halmet from "../../../public/assets/Vector8.png";
+import tea from "../../../public/assets/Vector9.png";
+import gift from "../../../public/assets/Vector10.png";
+import medicine from "../../../public/assets/Vector11.png";
+import azs from "../../../public/assets/Vector12.png";
+import education from "../../../public/assets/Vector13.png";
+import child from "../../../public/assets/Vector14.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Merchants = () => {
-	// const randArr = useState<(randArr:number[])>;
-	// const randArr = useState<number[]>([]);
-
-	return (
-		<section>
-			<div className="merch-container">
-				<div>
-					<h1 className="merch-headline">Где же оплатить?</h1>
-					<p className="merch-paragraph">Выберите категорию для того, чтобы посмотреть список магазинов</p>
-				</div>
-				<div className="merch-list">
-					<MerchBox text="Бытовая техника">
-						<Image src={tech} width={120} height={123} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Телефоны и аксессуары">
-						<Image src={phone} width={106} height={133} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Одежда и обувь">
-						<Image src={clothes} width={141} height={120} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Мебель и товары для дома">
-						<Image src={couch} width={133} height={106} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Компьютеры и оргтехника">
-						<Image src={comp} width={133} height={133} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Ювелирные изделия">
-						<Image src={ring} width={112} height={146} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Продукты">
-						<Image src={food} width={118} height={134} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Парфюмерия и косметика">
-						<Image src={parfum} width={132} height={126} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Стройматериалы">
-						<Image src={halmet} width={132} height={80} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Кафе и рестораны">
-						<Image src={tea} width={131} height={134} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Цветы и подарки">
-						<Image src={gift} width={126} height={134} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Медицинские услуги и лекарства">
-						<Image src={medicine} width={120} height={134} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="АЗС и автозапчасти">
-						<Image src={azs} width={143} height={138} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Образование">
-						<Image src={education} width={133} height={119} alt="2" className="merch-icon" />
-					</MerchBox>
-					<MerchBox text="Товары для детей">
-						<Image src={child} width={120} height={120} alt="2" className="merch-icon" />
-					</MerchBox>
-				</div>
-				<WhiteButton text="Посмотреть все магазины" />
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <div className="merch-container">
+        <motion.div
+          initial={{ margin: -50, opacity: 0 }}
+          whileInView={{ margin: 50, opacity: 1 }}
+          // transition={{ delay: 0.2 }}
+        >
+          <h1 className="merch-headline">Где же оплатить?</h1>
+          <p className="merch-paragraph">
+            Выберите категорию для того, чтобы посмотреть список магазинов
+          </p>
+        </motion.div>
+        <div className="merch-list">
+          <MerchBox text="Бытовая техника">
+            <Image
+              src={tech}
+              width={120}
+              height={123}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Телефоны и аксессуары">
+            <Image
+              src={phone}
+              width={106}
+              height={133}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Одежда и обувь">
+            <Image
+              src={clothes}
+              width={141}
+              height={120}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Мебель и товары для дома">
+            <Image
+              src={couch}
+              width={133}
+              height={106}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Компьютеры и оргтехника">
+            <Image
+              src={comp}
+              width={133}
+              height={133}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Ювелирные изделия">
+            <Image
+              src={ring}
+              width={112}
+              height={146}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Продукты">
+            <Image
+              src={food}
+              width={118}
+              height={134}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Парфюмерия и косметика">
+            <Image
+              src={parfum}
+              width={132}
+              height={126}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Стройматериалы">
+            <Image
+              src={halmet}
+              width={132}
+              height={80}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Кафе и рестораны">
+            <Image
+              src={tea}
+              width={131}
+              height={134}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Цветы и подарки">
+            <Image
+              src={gift}
+              width={126}
+              height={134}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Медицинские услуги и лекарства">
+            <Image
+              src={medicine}
+              width={120}
+              height={134}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="АЗС и автозапчасти">
+            <Image
+              src={azs}
+              width={143}
+              height={138}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Образование">
+            <Image
+              src={education}
+              width={133}
+              height={119}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+          <MerchBox text="Товары для детей">
+            <Image
+              src={child}
+              width={120}
+              height={120}
+              alt="2"
+              className="merch-icon"
+            />
+          </MerchBox>
+        </div>
+        <WhiteButton text="Посмотреть все магазины" />
+      </div>
+    </section>
+  );
 };
 
 export default Merchants;
