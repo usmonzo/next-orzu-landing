d9b33a85d8273866ac4343537e018a3168899ddd
usmonzo <turaef.usmon@gmail.com>
2023-06-01 00:03:16 +0500

    fix: select scroll bugs; styles: minor bugs fix;

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,42 +1,42 @@
-"use client";
-import styled from "@emotion/styled";
+'use client';
+import styled from '@emotion/styled';
 
 interface Props {
-  text: string;
-  aligned?: any;
-  justify?: any;
-  onClick?: Function;
-  zIndex?: number;
+	text: string;
+	aligned?: any;
+	justify?: any;
+	onClick?: Function;
+	zIndex?: number;
 }
 interface IComponent {
-  justifySelf: any;
-  align: any;
+	justifySelf: any;
+	align: any;
 }
 export const PrimaryButtonContainer = styled.button<IComponent>`
-  padding: 17px 30px;
-  gap: 12px;
-  border-radius: 50px;
-  background-color: #ff6200;
-  border: none;
-  font-weight: 500;
-  font-size: 1.2rem;
-  line-height: 20px;
-  text-align: center;
-  color: #ffffff;
-  z-index: ${(props: any) => props.zIndex || 0};
-  align-self: ${(props: any) => props.align || ""};
-  justify-self: ${(props: any) => props.justifySelf || ""};
+	padding: 17px 30px;
+	gap: 12px;
+	border-radius: 50px;
+	background-color: #ff6200;
+	border: none;
+	font-weight: 500;
+	font-size: 1.2rem;
+	line-height: 20px;
+	text-align: center;
+	color: #ffffff;
+	z-index: ${(props: any) => props.zIndex || 1};
+	align-self: ${(props: any) => props.align || ''};
+	justify-self: ${(props: any) => props.justifySelf || ''};
 
-  @media screen and (max-width: 735px) {
-    font-size: 1rem;
-  }
+	@media screen and (max-width: 735px) {
+		font-size: 1rem;
+	}
 `;
 const PrimaryButton = (props: Props) => {
-  return (
-    <PrimaryButtonContainer align={props.aligned} justifySelf={props.justify}>
-      {props.text}
-    </PrimaryButtonContainer>
-  );
+	return (
+		<PrimaryButtonContainer align={props.aligned} justifySelf={props.justify}>
+			{props.text}
+		</PrimaryButtonContainer>
+	);
 };
 
 export default PrimaryButton;
