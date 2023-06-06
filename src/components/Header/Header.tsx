dec23dbdaf8ec3a5f27d10832396261b5684e05b
usmonzo <turaef.usmon@gmail.com>
2023-06-06 18:38:32 +0500

    test: opacity animation [#2];

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,13 +14,35 @@ const Header = () => {
   const { scrollY } = useScroll();
   // const scrollRef = useRef(null);
   const [cardsOpacity, setCardsOpacity] = useState(1);
-  // const [prevOpacity];
+  const [cardTranslation, setCardTranslate] = useState({
+    x: 0,
+    y: 0,
+  });
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     console.log("Page scroll: ", latest);
-    if (latest >= 66 && latest <= 430) {
-      setCardsOpacity(1 - (latest - 66) / 365);
-      console.log(cardsOpacity);
+    if (latest >= 166 && latest <= 630) {
+      // 1 is start opacity point
+      // 66 animation start
+      // 430 end of animation
+      // 365 one tick of scroll
+      setCardsOpacity(1 - (latest - 166) / 465);
+      setCardTranslate({
+        x: -(((latest - 166) * 100) / 465),
+        y: -(((latest - 166) * 100) / 465),
+      });
+    } else if (latest > 630) {
+      setCardTranslate({
+        x: -100,
+        y: -100,
+      });
+      setCardsOpacity(0);
+    } else if (latest < 166) {
+      setCardsOpacity(1);
+      setCardTranslate({
+        x: 0,
+        y: 0,
+      });
     }
   });
   return (
@@ -35,50 +57,34 @@ const Header = () => {
             padding: "0 20px",
           }}
         >
-          <h1
-            className={styles.header_title}
-            // initial={{ opacity: 0 }}
-            // whileInView={{ opacity: 1 }}
-            // transition={{ delay: 0.1 }}
-          >
+          <h1 className={styles.header_title}>
             Выгодный кредит для <br />
             <span className={styles.text_gradient}>
               покупки бытовой техники
             </span>
           </h1>
-          <p
-            className={styles.header_paragraph}
-            // initial={{ opacity: 0 }}
-            // whileInView={{ opacity: 1 }}
-            // transition={{ delay: 0.1 }}
-          >
+          <p className={styles.header_paragraph}>
             Получите одобрение в течении дня и получайте деньги, не посещая офис
           </p>
         </div>
-        <div
-          style={{ display: "flex", flexDirection: "column", gap: "12px" }}
-          // initial={{ opacity: 0 }}
-          // whileInView={{ opacity: 1 }}
-          // transition={{ delay: 0.1 }}
-        >
+        <div style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
           <PrimaryButton text="Получить кредит" />
           <span style={{ color: "#ff6200", fontSize: "0.75rem" }}>
             *Физическая карта отличается от изображения на сайте
           </span>
         </div>
-        <div
-          className={styles.header_icons_container}
-          // initial={{ margin: 20, opacity: 0 }}
-          // whileInView={{ margin: 0, opacity: 1 }}
-          // transition={{ delay: 0.1 }}
-        >
+        <div className={styles.header_icons_container}>
           <Image
             src={white}
             alt="2"
             width={1650}
             height={800}
             className={styles.header_icons}
-            style={{ opacity: `${cardsOpacity}` }}
+            style={{
+              opacity: `${cardsOpacity}`,
+              transform: `translate3d(${cardTranslation.x}%, ${cardTranslation.y}%,0)`,
+              // transition: "all 0.1s linear",
+            }}
           />
           <Image
             src={black}
@@ -86,6 +92,11 @@ const Header = () => {
             width={1650}
             height={800}
             className={styles.header_icons}
+            style={{
+              opacity: `${cardsOpacity}`,
+              transform: `translate3d(${-cardTranslation.x}%, ${-cardTranslation.y}%,0)`,
+              // transition: "all 0.1s linear",
+            }}
           />
         </div>
       </header>
--- src/components/HeaderContent/HeaderContent.tsx
+++ src/components/HeaderContent/HeaderContent.tsx
@@ -3,33 +3,91 @@ import PrimaryButton from "../Buttons/PrimaryButton";
 import styles from "./HeaderContent.module.scss";
 import Image from "next/image";
 import timer from "../../../public/assets/timer.svg";
-import { Spinner } from "@chakra-ui/react";
-import { motion, useMotionValueEvent, useScroll } from "framer-motion";
-import { useRef, useState } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useCallback, useState } from "react";
 
 export default function HeaderContent() {
+  const { scrollY } = useScroll();
+  const [orangeContainerTranslate, setOrangeContainerTranslate] = useState(0);
+  const [grayContainerTranslate, setGrayContainerTranslate] = useState(0);
+  const [headlineTranslate, setHeadlineTranslate] = useState(780);
+  const [headlineBlur, setHeadlineBlur] = useState(4);
+  const [headlineOpacity, setHeadlineOpacity] = useState(1);
+  const [timerContainerTranslate, setTimerContainerTranslate] = useState(0);
+  const [blueContainerTranslate, setBlueContainerTranslate] = useState(0);
+  useMotionValueEvent(
+    scrollY,
+    "change",
+    useCallback((latest) => {
+      if (latest >= 200 && latest <= 480) {
+        setOrangeContainerTranslate(((latest - 200) * 500) / 280);
+        console.log(orangeContainerTranslate);
+      } else if (latest > 480) {
+        setOrangeContainerTranslate(500);
+      } else if (latest < 200) {
+        setOrangeContainerTranslate(0);
+      }
+      if (latest >= 350 && latest <= 480) {
+        setGrayContainerTranslate(((latest - 350) * 500) / 130);
+        console.log(orangeContainerTranslate);
+      } else if (latest > 480) {
+        setGrayContainerTranslate(500);
+      } else if (latest < 350) {
+        setGrayContainerTranslate(0);
+      }
+
+      if (latest >= 480 && latest <= 860) {
+        setHeadlineTranslate(780 - ((latest - 480) * 280) / 380);
+        setHeadlineBlur(4 - ((latest - 480) * 4) / 380);
+      } else if (latest >= 860 && latest <= 1200) {
+        setHeadlineTranslate(500 - ((latest - 860) * 500) / 340);
+        setHeadlineBlur(((latest - 860) * 4) / 340);
+        setHeadlineOpacity(1 - (latest - 860) / 340);
+      }
+
+      // else if (latest > 860) {
+      //   setHeadlineTranslate(500);
+      // }
+      // else if (latest < 480) {
+      //   setHeadlineTranslate(0);
+      // }
+      if (latest >= 700 && latest <= 1000) {
+        setTimerContainerTranslate(((latest - 700) * 550) / 300);
+      } else if (latest > 1000) {
+        setTimerContainerTranslate(550);
+      } else if (latest < 700) {
+        setTimerContainerTranslate(0);
+      }
+      if (latest >= 850 && latest <= 1000) {
+        setBlueContainerTranslate(((latest - 850) * 550) / 150);
+      } else if (latest > 1000) {
+        setBlueContainerTranslate(550);
+      } else if (latest < 850) {
+        setBlueContainerTranslate(0);
+      }
+    }, [])
+  );
   return (
     <>
-      <div
-        style={{ overflow: "scroll" }}
-        className={styles.header_main_content}
-      >
+      <div className={styles.header_main_content}>
         <div className={styles.main_content_horizontal}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            // whileHover={{ scale: 0.98 }}
+          <div
             className={styles.orange_box}
+            style={{
+              // transition: "all 0.1s linear",
+              transform: `translate3d(0px,${-orangeContainerTranslate}px,0px)`,
+            }}
           >
             <h2 className={styles.orange_box_text}>
               Получите кредит наличными почти сразу
             </h2>
             <BlackButton text={"Получить кредит"} />
-          </motion.div>
+          </div>
           <div
-            // whileHover={{ scale: 0.98 }}
-
             className={styles.white_box}
+            style={{
+              transform: `translate3d(0px,${-grayContainerTranslate}px,0px)`,
+            }}
           >
             <h2 className={styles.white_box_headline}>5 лет</h2>
             <div className={styles.wh}>
@@ -47,11 +105,23 @@ export default function HeaderContent() {
             />
           </div>
         </div>
-        <h1 className={styles.header_main_content_headline}>
+        <h1
+          className={styles.header_main_content_headline}
+          style={{
+            transform: `translate(0px,${-headlineTranslate}px)`,
+            filter: `blur(${headlineBlur.toFixed(3)}px)`,
+            opacity: `${headlineOpacity}`,
+          }}
+        >
           Пользуйтесь где угодно и когда угодно
         </h1>
         <div className={styles.main_content_horizontal}>
-          <div className={styles.white_box_timer}>
+          <div
+            className={styles.white_box_timer}
+            style={{
+              transform: `translate(0px,${-timerContainerTranslate}px)`,
+            }}
+          >
             <div style={{ zIndex: 5 }}>
               <span
                 style={{
@@ -74,7 +144,12 @@ export default function HeaderContent() {
               className={styles.white_box_timer_icon}
             />
           </div>
-          <div className={styles.blue_box}>
+          <div
+            className={styles.blue_box}
+            style={{
+              transform: `translate(0px,${-blueContainerTranslate}px)`,
+            }}
+          >
             <h2 className={styles.blue_box_text} style={{ fontWeight: "900" }}>
               Кредит <br />
               до
