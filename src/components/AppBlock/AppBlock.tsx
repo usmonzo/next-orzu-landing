169ab1e804d29f8ba7137a57ca6a0782498f5d3b
usmonzo <turaef.usmon@gmail.com>
2023-06-09 14:01:36 +0500

    add: animation to add block;

--- src/components/AppBlock/AppBlock.tsx
+++ src/components/AppBlock/AppBlock.tsx
@@ -6,8 +6,19 @@ import appgallery from "../../../public/assets/appgalery.svg";
 import elipsis from "../../../public/assets/ellipse.svg";
 import qrCode from "../../../public/assets/qrCode.svg";
 import orzuTheme from "../../../public/assets/orzuTheme2.svg";
+import {
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+  motion,
+} from "framer-motion";
 
 export const AppBlock = () => {
+  const { scrollYProgress } = useScroll();
+  const orzuTranslate = useTransform(scrollYProgress, [0.8, 0.82], [600, 0]);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+  });
   return (
     <div className={styles.app_color_block}>
       <div className={styles.app_text_container}>
@@ -47,13 +58,20 @@ export const AppBlock = () => {
           <p>Наведите камеру и скачайте бесплатное приложение Хумо Онлайн</p>
         </div>
       </div>
-      <Image
-        src={orzuTheme}
-        // width={1096}
-        // height={636}
-        alt="orzu"
+      <motion.div
         className={styles.block_orzu_theme}
-      />
+        style={{
+          translateX: orzuTranslate,
+          translateY: orzuTranslate,
+        }}
+      >
+        <Image
+          src={orzuTheme}
+          // width={1096}
+          // height={636}
+          alt="orzu"
+        />
+      </motion.div>
     </div>
   );
 };
